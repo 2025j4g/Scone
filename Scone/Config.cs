288daa8e365afa140178ad79ac2de7553db053c6
naviat-dev <sriemmanuel787@outlook.com>
2025-11-18 16:08:21 -0800

    Set default values in config

--- Scone/Config.cs
+++ Scone/Config.cs
@@ -2,7 +2,11 @@ namespace Scone;
 
 public struct Config
 {
-	public string? OutputDirectory;
-	public bool DarkMode;
-	public bool Gltf;
+	public string? OutputDirectory = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+	public int DarkMode = 2;
+	public bool Gltf = false;
+
+    public Config()
+    {
+    }
 }
